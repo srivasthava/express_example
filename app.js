fd56a29b2269772d9f74332db6b110b0e6e08e18
srivasthava <ssripada@deloitte.com>
2020-10-26 11:00:18 -0400

    Update app.js

--- app.js
+++ app.js
@@ -8,17 +8,17 @@ var routes = require('./routes/');
 
 // declare modules
 var account = require('./routes/findAccount');
-const { WebhookClient } = require("dialogflow-fulfillment");
-const { welcome, defaultFallback } = require("./intents/WelcomeExit");
+//const { WebhookClient } = require("dialogflow-fulfillment");
+//const { welcome, defaultFallback } = require("./intents/WelcomeExit");
 var app = express();
 
-app.post("/dialogflow", express.json(), (req, res) => {
-    const agent = new WebhookClient({ request: req, response: res });
-    let intentMap = new Map();
-    intentMap.set("Default Welcome Intent", welcome);
-    intentMap.set("Default Fallback Intent", defaultFallback);
-    agent.handleRequest(intentMap);
-});
+//app.post("/dialogflow", express.json(), (req, res) => {
+//    const agent = new WebhookClient({ request: req, response: res });
+//    let intentMap = new Map();
+//    intentMap.set("Default Welcome Intent", welcome);
+//    intentMap.set("Default Fallback Intent", defaultFallback);
+//    agent.handleRequest(intentMap);
+//});
 
 
 // view engine setup
