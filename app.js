45bfa6b5b3b82ff66d98632dc4f227e541b18294
Srivasta Sripada <ssripada@deloitte.com>
2020-10-26 13:54:09 -0400

    config path

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var routes = require('./routes/');
-var config = require('config');
+var config = require('./config');
 // declare modules
 var account = require('./routes/findAccount');
 
